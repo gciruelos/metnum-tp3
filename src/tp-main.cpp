a2a10f2e50e1e29b54af5a280fd0b949a4a9822b
gciruelos <gonzalo.ciruelos@gmail.com>
2015-10-27 03:22:02 -0300

    subo el tp-main, no pude hacer andar los scripts con octave, parece que solo andan con matlab

---
+++ src/tp-main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <stdlib.h>
+//#include "problema.h"
+
+
+enum metodo {
+    NNEIGHBOUR = 0,
+    LINEAL,
+    SPLINES
+};
+
+
+int main(int argc, char * argv[]){
+  /* argv[0] es el nombre del programa
+   * argv[1] es el archivo de input
+   * argv[2] es el archivo de output
+   * argv[3] es el metodo a ejecutar
+   * argv[4] es el cantidad de cuadros a agregar
+   */
+
+
+
+    char * aux;
+    enum metodo m = (enum metodo) strtol(argv[3], &aux, 10);
+    unsigned int cant_cuadros = (argv[4], &aux, 10);
+    unsigned int c, height, width, f;
+    // Este va a ser un vector de height*width entradas, y cada entrada va 
+    // a ser un vector de c(uadros) cantidad de entradas cada uno
+    std::vector<std::vector<unsigned int> > frames;
+    
+
+    // abrimos el archivo de parametro y cargamos todo
+    std::ifstream input_file(argv[1]);
+    input_file >> c >> height >> width >> f;
+
+    frames = std::vector<std::vector<unsigned int> >(height * width);
+
+    for(unsigned int pixel = 0; pixel < height * width; pixel++){
+        for(unsigned int cuadro = 0; cuadro < c; cuadro++){
+            unsigned int gris;
+            input_file >> gris;
+            frames[pixel].push_back(gris);
+        }
+    }
+    input_file.close();
+    
+    /*
+    // los achico para minimizar el uso de memoria (c++11)
+    for(unsigned int pixel = 0; pixel < height * width; pixel++)
+        frames[pixel].shrink_to_fit();
+    */
+
+    // abrimos el archivo de la red y lo cargamos 
+    // ACA YA ESTA TODO LISTO PARA SER USADO
+    std::ofstream solus_file(argv[2], std::ofstream::out);
+
+    
+
+  
+ 
+
+ 
+    return 0;
+}
